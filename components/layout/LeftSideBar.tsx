aeec2c51443043e5b30de8be56d98c411b08edac
devcowsar <kawsarcse836@gmail.com>
2024-06-01 10:16:39 +0600

    fix: Leftsidebar

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LeftSideBar = () => {
-  return <div>LeftSideBar</div>;
+  return (
+    <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-3 shadow-xl max-lg:hidden">
+      LeftSideBar
+    </div>
+  );
 };
 
 export default LeftSideBar;
