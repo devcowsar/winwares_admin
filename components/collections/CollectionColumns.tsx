3023fde55c95ae1f430cf034ea7eec45b84716d5
devcowsar <kawsarcse836@gmail.com>
2024-06-27 09:39:55 +0600

    fix: collectioncolumn_datatable,collectionpage,

---
+++ components/collections/CollectionColumns.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import Delete from "../custom ui/Delete";
+
+export const columns: ColumnDef<CollectionType>[] = [
+  {
+    accessorKey: "title",
+    header: "Title",
+    cell: ({ row }) => {
+      return <p className="text-red">{row.original.title}</p>;
+    },
+  },
+  {
+    accessorKey: "products",
+    header: "Prouducts",
+    cell: ({ row }) => {
+      return <p>{row.original.products.length}</p>;
+      console.log(row);
+    },
+  },
+  {
+    accessorKey: "actions",
+    cell: ({ row }) => <Delete />,
+  },
+];
---
+++ components/custom ui/Delete.tsx
@@ -0,0 +1,45 @@
+import { Trash } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { Button } from "../ui/button";
+
+const Delete = () => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button className="bg-red-1 text-white">
+          <Trash className="w-4 h-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-red-1">
+            Are you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            collection.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className="bg-red-1 text-white">
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default Delete;
