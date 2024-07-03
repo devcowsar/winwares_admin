e44fade7790bb0aa64d992cbe22dbab6a58ffe59
devcowsar <kawsarcse836@gmail.com>
2024-07-03 11:02:17 +0600

    fix: imageupload

--- components/custom ui/ImageUpload.tsx
+++ components/custom ui/ImageUpload.tsx
@@ -1,7 +1,7 @@
 import { CldUploadWidget } from "next-cloudinary";
-import { Button } from "../ui/button";
 import { Plus, Trash } from "lucide-react";
-import { FC } from "react";
+
+import { Button } from "../ui/button";
 import Image from "next/image";
 
 interface ImageUploadProps {
@@ -10,7 +10,11 @@ interface ImageUploadProps {
   onRemove: (value: string) => void;
 }
 
-const ImageUpload: FC<ImageUploadProps> = ({ onChange, onRemove, value }) => {
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  onChange,
+  onRemove,
+  value,
+}) => {
   const onUpload = (result: any) => {
     onChange(result.info.secure_url);
   };
@@ -19,31 +23,37 @@ const ImageUpload: FC<ImageUploadProps> = ({ onChange, onRemove, value }) => {
     <div>
       <div className="mb-4 flex flex-wrap items-center gap-4">
         {value.map((url) => (
-          <div className="relative w-[200px] h-[200px]" key={url}>
+          <div key={url} className="relative w-[200px] h-[200px]">
             <div className="absolute top-0 right-0 z-10">
               <Button
+                type="button"
                 onClick={() => onRemove(url)}
                 size="sm"
                 className="bg-red-1 text-white"
               >
-                <Trash calcMode="h-4 w-4" />
+                <Trash className="h-4 w-4" />
               </Button>
             </div>
             <Image
               src={url}
-              alt="collections"
+              alt="collection"
               className="object-cover rounded-lg"
               fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
         ))}
       </div>
+
       <CldUploadWidget uploadPreset="wrkibkmy" onUpload={onUpload}>
         {({ open }) => {
           return (
-            <Button onClick={() => open()} className="bg-grey-1 text-white">
-              <Plus className="h-4 w-4 mr-2" /> Upload Image
+            <Button
+              type="button"
+              onClick={() => open()}
+              className="bg-grey-1 text-white"
+            >
+              <Plus className="h-4 w-4 mr-2" />
+              Upload Image
             </Button>
           );
         }}
