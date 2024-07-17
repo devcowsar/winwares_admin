2f44224607510f2db49df3781a69e544ae4ef4dc
devcowsar <kawsarcse836@gmail.com>
2024-07-17 16:27:22 +0600

    fix: post_routes

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -1,6 +1,7 @@
 import Collection from "@/lib/models/Collection";
 import Product from "@/lib/models/Product";
 import { connectToDB } from "@/lib/mongoDb";
+import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest, { params }: { params: { productId: string } }) => {
@@ -19,3 +20,89 @@ export const GET = async (req: NextRequest, { params }: { params: { productId: s
   }
 }
 
+export const POST = async (req:NextRequest,{params}:{params:{productId:string}})=>{
+  try{
+    const {userId}=auth()
+    
+    if(!userId){
+      return new NextResponse("Unauthorized",{status:401})
+
+    }
+    await connectToDB()
+
+    const product = await Product.findById(params.productId);
+    if(!product){
+      return new NextResponse(JSON.stringify({message:"Product not found"}),{status:404})
+    }
+    const {
+      title,
+      description,
+      media,
+      category,
+      collections,
+      tags,
+      sizes,
+      colors,
+      price,
+      expense,
+    } = await req.json();
+
+    if (!title || !description || !media || !category || !price || !expense) {
+      return new NextResponse("Not enough data to create a new product", {
+        status: 400,
+      });
+    }
+    const addedCollections = collections.filter(
+      (collectionId: string) => !product.collections.includes(collectionId)
+    );
+
+    const removedCollections = product.collections.filter(
+      (collectionId: string) => !collections.includes(collectionId)
+    );
+    
+
+    //update collection
+
+    await Promise.all([
+      ...addedCollections.map((collectionId:string)=>
+      Collection.findByIdAndUpdate(collectionId,{
+        $push:{products:product._id}
+      })),
+
+      ...removedCollections.map((collectionId:string)=>Collection.findByIdAndUpdate(
+        collectionId,{
+          $pull:{products:product._id}
+        }
+      ))
+    ]);
+
+    //updata product
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      product._id,
+      {
+        title,
+        description,
+        media,
+        category,
+        collections,
+        tags,
+        sizes,
+        colors,
+        price,
+        expense,
+      },
+      { new: true }
+    ).populate({ path: "collections", model: Collection });
+
+    await updatedProduct.save();
+
+    return NextResponse.json(updatedProduct,{status:200})
+
+
+  }catch(err){
+    console.log("[prouductId_POST]",err)
+    return new NextResponse("Internal error",{status:500})
+  }
+
+}
