7478fbb6a9c7f9a0c04339fbc90430b8192bf40c
devcowsar <kawsarcse836@gmail.com>
2024-08-03 00:36:41 +0600

    fix: orders

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,12 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
 import { DataTable } from "@/components/custom ui/DataTable";
 import { columns } from "@/components/orders/OrderColumns";
 import { Separator } from "@/components/ui/separator";
+import Loader from "@/components/custom ui/Loader";
 
-const Orders = async () => {
-  const res = await fetch("http://localhost:3000/api/orders");
-  const orders = await res.json();
-  console.log(orders);
-  return (
+const Orders = () => {
+  const [loading, setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
+  const getOrders = async () => {
+    try {
+      const res = await fetch(`/api/orders`);
+      const data = await res.json();
+      setOrders(data);
+      setLoading(false);
+    } catch (err) {
+      console.log("[orders_GET]", err);
+    }
+  };
+  useEffect(() => {
+    getOrders();
+  }, []);
+  return loading ? (
+    <Loader />
+  ) : (
     <div className="px-10 py-5">
       <p className="text-heading2-bold">Orders</p>
       <Separator className="bg-grey-1 my-5" />
