870b052d0dafd9b8d0ef2335e29c0802aa578238
devcowsar <kawsarcse836@gmail.com>
2024-08-02 15:21:01 +0600

    fix: orderdetails

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -1,5 +1,32 @@
-const OrderDetails = () => {
-  return <div>OrderDetails</div>;
+const OrderDetails = async ({ params }: { params: { orderId: String } }) => {
+  const res = await fetch(`http://localhost:3000/api/orders/${params.orderId}`);
+  const { orderDetails, customer } = await res.json();
+  const { street, city, state, postalCode, country } =
+    orderDetails.shippingAddress;
+  return (
+    <div className="flex flex-col p-10 gap-5">
+      <p className="text-base-bold">
+        Order ID:<span className="text-base-medium">{orderDetails._id}</span>
+      </p>
+      <p className="text-base-bold">
+        Customer Name:<span className="text-base-medium">{customer.name}</span>
+      </p>
+      <p className="text-base-bold">
+        Shipping address:
+        <span className="text-base-medium">
+          {street},{city},{state},{postalCode},{country}
+        </span>
+      </p>
+      <p className="text-base-bold">
+        Total Paid:
+        <span className="text-base-medium">{orderDetails.totalAmount}</span>
+      </p>
+      <p className="text-base-bold">
+        Shipping rate ID:
+        <span className="text-base-medium">{orderDetails.shippingRate}</span>
+      </p>
+    </div>
+  );
 };
 
 export default OrderDetails;
