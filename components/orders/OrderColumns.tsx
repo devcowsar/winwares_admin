17c0b7fe9e66d4e3eba960286cf900d9985a256d
devcowsar <kawsarcse836@gmail.com>
2024-08-02 22:26:46 +0600

    feat: orderitemcolumns

--- components/orders/OrderColumns.tsx
+++ components/orders/OrderColumns.tsx
@@ -9,7 +9,7 @@ export const columns: ColumnDef<OrderColumnType>[] = [
     header: "Order",
     cell: ({ row }) => {
       return (
-        <Link href={`orders/${row.original._id}`} className="hover:text-red-1">
+        <Link href={`/orders/${row.original._id}`} className="hover:text-red-1">
           {row.original._id}
         </Link>
       );
